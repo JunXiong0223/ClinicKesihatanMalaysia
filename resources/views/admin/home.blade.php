87c7c446745e42207ed2271d5be04e1d2d35356f
JunXiong0223 <88370615+JunXiong0223@users.noreply.github.com>
2022-10-27 21:12:19 +0800

    dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -10,6 +10,72 @@
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 offset-0 offset-sm-0 offset-md-0 offset-lg-0 offset-xl-0" style="margin-top: 5px;margin-bottom: 5px;">
             <p><br>List of staff<br><br></p>
         </div>
+        <div class="row">
+            <div class="col-9 d-flex d-sm-flex d-md-flex d-lg-flex d-xl-flex flex-fill justify-content-center justify-content-sm-center justify-content-md-center align-items-md-center justify-content-lg-center" style="margin-top: 5px;margin-bottom: 5px;">
+                <div class="d-sm-flex flex-fill justify-content-sm-center align-items-sm-center"></div>
+            </div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12 d-md-flex flex-fill justify-content-md-center align-items-md-center" style="padding: 10px;">
+                        <div class="card" style="border-radius: 18px;">
+                            <div class="card-body" style="border-radius: -1px;">
+                                <h4 class="card-title">Title</h4>
+                                <p class="text-break card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 d-md-flex flex-fill justify-content-md-center align-items-md-center" style="padding: 10px;">
+                        <div class="card" style="border-radius: 18px;">
+                            <div class="card-body" style="border-radius: -1px;">
+                                <h4 class="card-title">Title</h4>
+                                <p class="text-break card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 d-md-flex flex-fill justify-content-md-center align-items-md-center" style="padding: 10px;">
+                        <div class="card" style="border-radius: 18px;">
+                            <div class="card-body" style="border-radius: -1px;">
+                                <h4 class="card-title">Title</h4>
+                                <p class="text-break card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+                <div class="row">
+                    <div class="col-12 d-md-flex flex-fill justify-content-md-center align-items-md-center" style="padding: 10px;">
+                        <div class="card" style="border-radius: 18px;">
+                            <div class="card-body" style="border-radius: -1px;">
+                                <h4 class="card-title">Title</h4>
+                                <p class="text-break card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 d-md-flex flex-fill justify-content-md-center align-items-md-center" style="padding: 10px;">
+                        <div class="card" style="border-radius: 18px;">
+                            <div class="card-body" style="border-radius: -1px;">
+                                <h4 class="card-title">Title</h4>
+                                <p class="text-break card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 d-md-flex flex-fill justify-content-md-center align-items-md-center" style="padding: 10px;">
+                        <div class="card" style="border-radius: 18px;">
+                            <div class="card-body" style="border-radius: -1px;">
+                                <h4 class="card-title">Title</h4>
+                                <p class="text-break card-text">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9 d-flex d-sm-flex d-md-flex d-lg-flex d-xl-flex flex-fill justify-content-center justify-content-sm-center justify-content-md-center align-items-md-center justify-content-lg-center" style="margin-top: 5px;margin-bottom: 5px;">
+                <div class="d-sm-flex flex-fill justify-content-sm-center align-items-sm-center"></div>
+            </div>
+        </div>
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 offset-0 offset-sm-0 offset-md-0 offset-lg-0 offset-xl-0" style="margin-top: 5px;margin-bottom: 5px;"></div>
         <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 offset-0 offset-sm-0 offset-md-0 offset-lg-0 offset-xl-0" style="margin-top: 5px;margin-bottom: 5px;"></div>
     </div>
