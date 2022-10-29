de3168a36ae3410973bd68d05fef9717670c2327
JunXiong0223 <88370615+JunXiong0223@users.noreply.github.com>
2022-10-29 17:48:50 +0800

    vendor file

---
+++ app/Http/Controllers/PHPMailerController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+class PHPMailerController extends Controller
+{
+    //
+}
