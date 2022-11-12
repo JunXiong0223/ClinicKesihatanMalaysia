194c8c2378c59bda9e3d8595bd02f57726f720e7
JunXiong0223 <88370615+JunXiong0223@users.noreply.github.com>
2022-11-12 13:27:47 +0800

    Update UI

--- resources/views/user/clinicDetail.blade.php
+++ resources/views/user/clinicDetail.blade.php
@@ -180,19 +180,7 @@
             </form>
         </div>
     </div>
-    <div class="modal fade" role="dialog" tabindex="-1" id="successful_appointment">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Modal Title</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Appointment message</p>
-                </div>
-                <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">Close</button></div>
-            </div>
-        </div>
-    </div>
+    
 @endsection
 
 @section('js')
